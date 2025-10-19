0e1438fd08ff76101c4507056c5f2b042609b200
Lauret <currilauret@gmail.com>
2025-10-19 14:57:19 +0100

    Added RandomInput

---
+++ Random_Input.py
@@ -0,0 +1,25 @@
+import random
+import keyboard
+import time
+
+
+InputsPosibles = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+    'u', 'v', 'w', 'x', 'y', 'z',
+
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+
+    'space', 'left', 'right', 'up', 'down',
+
+    'comma', 'period', 'slash', 'semicolon', 'backslash', 'equal'
+]
+
+def Randomizar():
+    RandomKey = random.choice(InputsPosibles)
+    keyboard.press(RandomKey)
+    keyboard.release(RandomKey)
+    time.sleep(.01)
+
+while True:
+    Randomizar()
