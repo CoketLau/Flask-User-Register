3e23882e2fca4ea8a304366c24f5ff56ac818310
Lauret <currilauret@gmail.com>
2025-11-05 16:55:14 +0000

    Fixed the fucking bug ong

--- Login/blueprints/logout.py
+++ Login/blueprints/logout.py
@@ -2,7 +2,7 @@ from flask import Blueprint, redirect, url_for, session, render_template, flash
 from DataBase.models import app
 
 
-bp = Blueprint("delete", __name__)
+bp = Blueprint("logout", __name__)
 
 
 @app.route("/logout")
